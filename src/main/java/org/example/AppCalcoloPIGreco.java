fd63e619227c5bad50cc9f27d381823cfb9179fe
Massimo Cappellano <massimo.cappe@gmail.com>
2023-12-19 12:20:47 +0100

    improv

--- src/main/java/org/example/AppCalcoloPIGreco.java
+++ src/main/java/org/example/AppCalcoloPIGreco.java
@@ -4,14 +4,13 @@ public class AppCalcoloPIGreco {
 
     public static void main(String[] args) {
 
-        WorkerThread worker = new WorkerThread(0.0000001);
+        WorkerThread worker = new WorkerThread(0.000000001);
         worker.start();
 
         InputHandlerThread inputHandler = new InputHandlerThread(worker);
         inputHandler.start();
 
         double result = worker.getResult();
-        System.out.println("SONO QUI!!!");
         inputHandler.doStop();
 
         System.out.println(result + " - difference " + (Math.PI - result));
--- src/main/java/org/example/InputHandlerThread.java
+++ src/main/java/org/example/InputHandlerThread.java
@@ -1,29 +1,49 @@
 package org.example;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 
 public class InputHandlerThread extends Thread {
 
     private Thread wk;
-    private Scanner sc;
+    private volatile boolean attivo;
+
+    private BufferedReader br;
     public InputHandlerThread(Thread wk) {
         this.wk = wk;
     }
 
     public void run() {
+        attivo = true;
         System.out.println("Digita per interrompere il calcolo!!");
 
-        sc = new Scanner(System.in);
-        if (sc.hasNextLine()) {
-            System.out.println("DIGITATO");
-            wk.interrupt();
-        }
+        br = new BufferedReader(new InputStreamReader(System.in));
 
+            try {
+                // wait until we have data to complete a readLine()
+                while (!br.ready() && attivo ) {
+                    Thread.sleep(10);
+                }
+
+                if(!attivo) {
+                    br.close();
+                    return;
+                }
+
+
+                String input = br.readLine();
+                System.out.println("DIGITATO");
+                wk.interrupt();
+                br.close();
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
     }
 
     public void doStop() {
-      if(sc!= null)
-          sc.close();
+      attivo = false;
     }
 }
